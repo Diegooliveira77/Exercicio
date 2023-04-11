24d9436d140e55f01ed14517144e609a635ac250
Diego <Diego@DESKTOP-VKT3JJP>
2023-04-10 21:36:14 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorDePalavras = 0;
+
+            Console.WriteLine("Digite uma frase: ");
+            string frase = Console.ReadLine();
+
+            string[] palavras = frase.Split(' ');
+
+            for (int i = 0; i < palavras.Length; i++)
+            {
+                if (palavras[i] == " ")
+                {
+                    continue;
+                }
+                else
+                {
+                    contadorDePalavras++;
+                }
+
+                Console.WriteLine($" Numero de palavras: {contadorDePalavras}");
+            }
+        }
+    }
+}
